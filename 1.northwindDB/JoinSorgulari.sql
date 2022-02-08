2eaa08ee20e61981aa76dc0248e47446017737f6
iskyldrm <isakyildirim@outlook.com>
2022-02-08 16:50:52 +0300

    2.soruçözüldü

--- 1.northwindDB/JoinSorgulari.sql
+++ 1.northwindDB/JoinSorgulari.sql
@@ -3,3 +3,14 @@ from Orders o
 inner join Employees e on o.EmployeeId=e.EmployeeId
 Group by e.FirstName ,e.LAstName ,year(o.OrderDate)
 Order by e.FirstName ,Yil 
+
+
+--2: müşlterilerimin 1997 1. çeyrekte verdiği sipariş sayısı
+
+
+select c.CompanyName,count(o.OrderID) as siparissayisi
+from Customers c
+inner join Orders o on o.CustomerID=c.CustomerID
+where year(o.OrderDate) = 1997
+and MONTH(o.OrderDate) between 1 and 3 
+group by c.CompanyName
