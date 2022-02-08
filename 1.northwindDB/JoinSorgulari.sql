e7323a0c765ff75757772b285cb326a1840ac3aa
iskyldrm <isakyildirim@outlook.com>
2022-02-08 16:32:37 +0300

    1. soru çözüldü

---
+++ 1.northwindDB/JoinSorgulari.sql
@@ -0,0 +1,5 @@
+﻿select Categories.CategoryName,count(Products.ProductID) urunler
+from Products,Categories
+where  Products.CategoryID=Categories.CategoryID
+group by Categories.CategoryName
+order by 2 desc
