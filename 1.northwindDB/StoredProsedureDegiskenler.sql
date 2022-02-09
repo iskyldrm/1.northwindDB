f5b1ad2018d50f22a9f290c090d854ac85c0b179
iskyldrm <isakyildirim@outlook.com>
2022-02-09 16:45:15 +0300

    Prosedur oluturma anlatıldı

--- 1.northwindDB/StoredProsedureDegiskenler.sql
+++ 1.northwindDB/StoredProsedureDegiskenler.sql
@@ -75,3 +75,33 @@ Select @adi ='ali',@soyadi = 'veli',@yas = '30'
 select @adi adi,@soyadi soyadi,@yas yas
 
 print @adi + @soyadi
+
+/*
+
+Prosedure olulturmak için creaat komutu kulanılır. Creat komutu
+genel anlamda sql server içerisinde oluşturalabilcek objelerden sorumludur
+
+Creat 
+
+*/
+
+
+ALTER procedure DortIslem
+(@sayi1 int,
+@sayi2 int)
+as
+begin
+--declare @sonuc float
+
+--set @sonuc = @sayi1/@sayi2
+
+print 'Çarpım :' + CONVERT(VARCHAR(50),(@sayi1*@sayi2))
+--print 'Bölme :' + (@sayi1/@sayi2)
+
+--print 'Toplama :' + (@sayi1+@sayi2)
+
+--print 'Çıkarma :' + (@sayi1 - @sayi2)
+
+end
+
+
