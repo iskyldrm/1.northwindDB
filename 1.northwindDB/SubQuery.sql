41d857f528f138e8324c20ac0758275781648099
iskyldrm <isakyildirim@outlook.com>
2022-02-11 17:29:03 +0300

    subquery ornekleri yapıldı

---
+++ 1.northwindDB/SubQuery.sql
@@ -0,0 +1,23 @@
+﻿
+-- en pahalı ürünüm hangisidir
+
+select top 1* from Products
+
+order by UnitPrice desc
+
+
+select * from Products 
+where UnitPrice=(select max(UnitPrice) from Products)
+
+select * from Products
+where UnitPrice>(select avg(UnitPrice) from Products)
+
+
+select productname, (select CategoryName from Categories
+			where CategoryID=Products.CategoryID) categoryname, unitprice from Products
+
+
+
+select top 1 FirstName, len(FirstName), (select top 1 FirstName, LEN(FirstName) from Employees order by 2 desc)
+from Employees
+order by 2  
