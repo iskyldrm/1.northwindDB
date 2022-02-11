b303a7317d7252df0bc592ea8fadad6e9ba420cb
iskyldrm <isakyildirim@outlook.com>
2022-02-11 17:52:31 +0300

    DynamicsSQL oluşturuldu

---
+++ 1.northwindDB/Dynamiccssql.sql
@@ -0,0 +1,14 @@
+﻿
+
+declare @yil int
+declare @sql nvarchar(max)
+set @yil = 1998
+set @sql = 'Select * from Orders'
+if(@yil>1990)
+ set @sql = @sql + ' Where year(orderDate)='+convert(nvarchar,@yil) 
+ print @sql
+--exec sp_executesql @statement = @sql
+Exec(@sql)
+exec sys.sp_server_info
+exec sys.sp_tables 
+
